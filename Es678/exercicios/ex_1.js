ad38df42c1514c220a442c7c94e22ca60111c1c5
RodrigoBJ <rodrigojeansantos@gmail.com>
2019-08-24 19:03:03 -0300

    Fazer os exercicios feliz

---
+++ Es678/exercicios/ex_1.js
@@ -0,0 +1,25 @@
+const escola = [
+  { nome: 'Turma M1', alunos: [ { nome: 'Yamcha', nota: 5.2 }, { nome: 'Cell', nota: 8.0}] }, 
+  { nome: 'Turma M2', alunos: [{ nome: 'Kuririn', nota: 4.3 }, { nome: 'Bulma', nota: 10.0 }] },
+  { nome: 'Turma M3', alunos: [{ nome: 'Goku', nota: 4.1 }, { nome: 'Vegeta', nota: 7.3 }] },
+  { nome: 'Turma M4', alunos: [{ nome: 'Picollo', nota: 6.8 }, { nome: 'Gohan', nota: 6.1 }] },
+  { nome: 'Turma M5', alunos: [{ nome: 'MaginBoo', nota: 2.2 }, { nome: 'Mestre Kame', nota: 9.0 }] },
+  { nome: 'Turma M6', alunos: [{ nome: 'Brolly', nota: 1.2 }, { nome: 'Mr Sata', nota: 2.5 }] },
+  { nome: 'Turma M7', alunos: [{ nome: 'Freeza', nota: 7.8 }, { nome: 'Goku BLack', nota: 2.1 }] },
+]
+
+/*
+1) Obter a nota de todos os alunos e tirar uma média. Usar map para correr o objeto
+*/
+
+/*
+2) Obter a nota de todas as turmas e uma média. Usar map para correr o objeto
+*/
+
+/*
+3) Obter a maior nota dos alunos
+*/
+
+/*
+4) Obter um array com o melhor aluno e o pior
+*/
---
+++ Es678/exercicios/ex_2.js
@@ -0,0 +1,5 @@
+const nums = [1, 2, 3, 4, 5]
+
+/*
+1) Realizar a soma de todos os itens do array com map
+*/
